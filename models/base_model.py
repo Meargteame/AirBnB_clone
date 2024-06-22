813d1233812ef8d0639f0bbc87ae6953f93686ec
Meargteame <mearegteame99995555@gmail.com>
2024-06-22 23:38:39 +0300

    update

--- models/base_model.py
+++ models/base_model.py
@@ -17,13 +17,29 @@ class BaseModel:
         updated_at (datetime): Date and time of instance update.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of BaseModel.
+
+        Args:
+            kwargs (dict): Dictionary containing attributes and values to initialize the instance.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key != '__class__':
+                    setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
     
     def __str__(self):
         """
