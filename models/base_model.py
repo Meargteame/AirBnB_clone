5e6e47e6cbbda970b2c038ae709abc29bb274d77
Meargteame <mearegteame99995555@gmail.com>
2024-06-24 20:30:07 +0300

    BaseModel class updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,57 +1,48 @@
-#!usr/bin/env python3 
-
+#!/usr/bin/env python3
+"""
+BaseModel module for defining the base class for all models.
 """
 
-"""
 import uuid
-import datetime
+from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        """
+    """Base class for all models."""
 
-        """ 
-        self.id = uuid.uuid4()
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at 
+    def __init__(self, *args, **kwargs):
+        """Initialize a new instance.
         
-    def save(self):
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments. Used for deserialization.
         """
-        
-        """
-
-        self.updated_at= datetime.datetime.now()
-
-    
-    def to_dict(self):
-        """
-        
-        """
-
-        obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["create_at"] = self.created_at
-        obj_dict["updated_at"] = self.updated_at 
-        return obj_dict 
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+            self.created_at = datetime.fromisoformat(self.created_at)
+            self.updated_at = datetime.fromisoformat(self.updated_at)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
-        """
+        """Return a string representation of the instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Update the updated_at attribute with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary representation of the instance.
         
+        Returns:
+            dict: Dictionary representation of the instance.
         """
-
-        class_name = self.__class__.__name__
-
-        return f"{class_name} {self.id} {self.__dict__}"
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
+        dict_repr = self.__dict__.copy()
+        dict_repr['__class__'] = self.__class__.__name__
+        dict_repr['created_at'] = self.created_at.isoformat()
+        dict_repr['updated_at'] = self.updated_at.isoformat()
+        return dict_repr
