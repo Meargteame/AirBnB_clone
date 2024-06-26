211a0211d1b81f345bc55e70b4addcf0008356f0
Meargteame <mearegteame99995555@gmail.com>
2024-06-27 00:35:56 +0300

    base model updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,8 @@
-#!/usr/bin/ env python3 
+#!/usr/bin/env python3
 
 from uuid import uuid4
 from datetime import datetime
+
 class BaseModel:
     """
     Base model class that defines common attributes/methods for other classes.
@@ -12,18 +13,23 @@ class BaseModel:
         updated_at (str): Timestamp when the instance was last updated.
     """
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         """
         Initialize a new instance of BaseModel.
         """
-        self.id = str(uuid4())
-        self.created_at = (datetime.now())
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
         Return a string representation of the instance.
-        
+
         Returns:
             str: String representation of the instance.
         """
@@ -33,17 +39,41 @@ class BaseModel:
         """
         Update the updated_at attribute with the current datetime.
         """
-        self.updated_at = (datetime.now())
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
         Convert the instance attributes to a dictionary representation.
-        
+
         Returns:
             dict: Dictionary containing the instance attributes and class name.
         """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = str(self.created_at.isoformat())
-        obj_dict['updated_at'] = str(self.updated_at.isoformat())
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
+
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)
+
