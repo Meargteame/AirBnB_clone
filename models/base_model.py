fe971b59954ca3a876ffe0fd220e1c0519f8856b
Meargteame <mearegteame99995555@gmail.com>
2024-06-26 23:37:25 +0300

    BaseModel class updated

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,52 @@
-from uuid import uuid4 
-from datetime import datetime 
+#!/usr/bin/ env python3 
+
+from uuid import uuid4
+from datetime import datetime
+
 
 class BaseModel:
-           # the instance attributes and methods are in the below methods the so called the init method
-            
-           def __init__(self):
-               self.id =str(uuid4())
-               self.created_at = str(datetime.now())
-               self.updated_at = str(self.created_at)
-           def __str__(self):
-           #__str__: should print: [<class name>] (<self.id>) <self.__dict__>
-                return f"{self.__class__.__name__} {self.id} {self.__dict__}"
-           def save(self):
-                 self.updated_at = datetime.now()
-    
-           def to_dict(self):
-           # the first step of the serilizaation process chaning the object to dictionary representation
-           
-                obj_dict = self.__dict__
-                obj_dict[__class__ ] = self.__class__.__name__
-                self.created_at = self.created_at.isofromat()
-                self.created_at = self.updated_at.isoformat()
-                
-                return obj_dict
-            
-    
-my_model = BaseModel()
-print(my_model)
+    """
+    Base model class that defines common attributes/methods for other classes.
+
+    Attributes:
+        id (str): Unique identifier for the instance.
+        created_at (str): Timestamp when the instance was created.
+        updated_at (str): Timestamp when the instance was last updated.
+    """
+
+    def __init__(self):
+        """
+        Initialize a new instance of BaseModel.
+        """
+        self.id = str(uuid4())
+        self.created_at = str(datetime.now())
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """
+        Return a string representation of the instance.
+        
+        Returns:
+            str: String representation of the instance.
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        Update the updated_at attribute with the current datetime.
+        """
+        self.updated_at = str(datetime.now())
+
+    def to_dict(self):
+        """
+        Convert the instance attributes to a dictionary representation.
+        
+        Returns:
+            dict: Dictionary containing the instance attributes and class name.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at
+        obj_dict['updated_at'] = self.updated_at
+        return obj_dict
+
