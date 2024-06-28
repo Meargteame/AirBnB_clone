d4d62be4e558cb2eb1abe72647a86c5f25a0d365
Meargteame <mearegteame99995555@gmail.com>
2024-06-28 12:42:42 +0300

    console.py updated

--- console.py
+++ console.py
@@ -1,25 +1,34 @@
-import cmd 
+#!/usr/bin/python3
+"""
+This module contains the HBNBCommand class which inherits from cmd.Cmd.
+The HBNBCommand class defines the command interpreter for the HBNB project.
+"""
+
+import cmd
+
 class HBNBCommand(cmd.Cmd):
-      prompt ='( hbnb )'
-      def do_quite(self,line):
-            """ 
-            
-            """
-            return True 
-      def do_EOF(self,line):
-            """
-            
-            """ 
-            return True 
-      
-      # override the default empty line 
-      def emptyline(self):
-            """
-            
-            """ 
-            pass 
+    """
+    Command interpreter class for HBNB.
+    """
+    prompt = '(hbnb) '
+
+    def do_quit(self, line):
+        """
+        Quit command to exit the program.
+        """
+        return True
+
+    def do_EOF(self, line):
+        """
+        EOF command to exit the program.
+        """
+        return True
+
+    def emptyline(self):
+        """
+        Do nothing on empty input line.
+        """
+        pass
 
 if __name__ == '__main__':
-      HBNBCommand().cmdloop()
-      
-      
+    HBNBCommand().cmdloop()
