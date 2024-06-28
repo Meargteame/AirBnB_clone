4a40192574e76b655697d5a6ca3aad4e10e2f940
Meargteame <mearegteame99995555@gmail.com>
2024-06-28 13:04:12 +0300

    create command added to the console

--- console.py
+++ console.py
@@ -3,7 +3,8 @@
 This module contains the HBNBCommand class which inherits from cmd.Cmd.
 The HBNBCommand class defines the command interpreter for the HBNB project.
 """
-
+from models import storage
+from models.base_model import BaseModel
 import cmd
 
 class HBNBCommand(cmd.Cmd):
@@ -29,6 +30,21 @@ class HBNBCommand(cmd.Cmd):
         Do nothing on empty input line.
         """
         pass
+    def do_create(self,arg):
+         if not arg:
+               print('** class name missing **')
+               return 
+         try:
+            cls = globals()[arg]
+            if not issubclass(cls, BaseModel):
+                raise KeyError
+         except KeyError:
+            print("** class doesn't exist **")
+            return
+        
+         new_instance = cls()
+         new_instance.save()
+         print(new_instance.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
